568c79a7913fe34a94b1edddde948ee82cfb81d5
Eduardo Aguilar <dante.aguilar41@gmail.com>
2023-08-17 13:51:38 -0600

    fix: malformed response by extra field on api gateway v1

--- types/api_gateway_response.go
+++ types/api_gateway_response.go
@@ -10,3 +10,24 @@ type APIGatewayResponse struct {
 	// Just for APIGateway v2
 	Cookies []string `json:"cookies"`
 }
+
+func (agr APIGatewayResponse) ToV1Map() map[string]any {
+	return map[string]any{
+		"statusCode":        agr.StatusCode,
+		"headers":           agr.Headers,
+		"multiValueHeaders": agr.MultiValueHeaders,
+		"body":              agr.Body,
+		"isBase64Encoded":   agr.IsBase64Encoded,
+	}
+}
+
+func (agr APIGatewayResponse) ToV2Map() map[string]any {
+	return map[string]any{
+		"statusCode":        agr.StatusCode,
+		"headers":           agr.Headers,
+		"multiValueHeaders": agr.MultiValueHeaders,
+		"body":              agr.Body,
+		"isBase64Encoded":   agr.IsBase64Encoded,
+		"cookies":           agr.Cookies,
+	}
+}
